3b5de3e56be1f4776cdf45fe1230c2fc7915fb85
Randomgituser69 <159695185+Randomgituser69@users.noreply.github.com>
2024-09-15 14:47:19 +0000

    g g

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const btn = document.getElementById("btn")
 const color = document.querySelector(".color")
 
 btn.addEventListener('click', function() {
-    const randomNumber = 2;
-    document.body.style.backgroundColor = colors[randomNumber]
+    const randomNumber = getRamdomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
-})
+});
 
 function getRandomNumber() {
-    return Math.random();
+    return Math.floor(Math.random() * colors.length);
 }
